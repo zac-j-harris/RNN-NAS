83c591941c1575e2becd9694981bbee86a6c3a00
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2021-04-22 15:20:51 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,10 +3,12 @@
 #SBATCH --job-name=rnn-alg
 #SBATCH --account=mallet
 #SBATCH --time=0-00:01:00
+#SBATCH -o /home/zharris1/Documents/Jobs/RNN-NAS/out.txt
+#SBATCH -e /home/zharris1/Documents/Jobs/RNN-NAS/err.txt
 ###
 
 
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4
 
-python lstm.py
+python "/home/zharris1/Documents/Github/RNN-NAS/lstm.py"
