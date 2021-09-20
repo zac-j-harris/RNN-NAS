28caa7b114c26013df2a64d75fb75fc8a52a935a
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2021-09-20 12:31:20 -0600

    Update Model.py

--- Model.py
+++ Model.py
@@ -147,7 +147,7 @@ class Model():
 				new_layer_types[i] = layer_type
 				new_layer_specs[i] = self.random_init_values()
 				if layer_i == len(self.layer_types):
-					new_layer_specs[i][4] = prior_layer_out
+					new_layer_specs[i][4] = [1, 6]
 				new_input_shapes[i] = new_input_shape
 				# new_input_shapes[i] = (1, new_layer_specs[i - 1][4])
 			else:
