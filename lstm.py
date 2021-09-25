deb00e5443be5d75aec4b0c1e1b43cabe861f3d2
zac-j-harris <54678510+zac-j-harris@users.noreply.github.com>
2021-09-25 12:33:06 -0600

    Update lstm.py

--- lstm.py
+++ lstm.py
@@ -251,7 +251,7 @@ if __name__ == "__main__":
 	# As described in paper (crossover rate and mutation percentage differ)
 	if SERVER: #
 		# hyperparameters = {'generations': 30, 'pop_size': 20, 'mutation_rate': 0.30, 'mutation_percentage': 0.05,'elitism_rate': 0.1, 'structure_rate': 0.1}
-		hyperparameters = {'generations': 30, 'pop_size': 20, 'mutation_rate': 0.30, 'mutation_percentage': 0.05,'elitism_rate': 0.1, 'structure_rate': 0.33}
+		hyperparameters = {'generations': 30, 'pop_size': 20, 'mutation_rate': 0.30, 'mutation_percentage': 0.15,'elitism_rate': 0.1, 'structure_rate': 0.33}
 		epochs = 100
 		# hyperparameters = {'generations': 100, 'pop_size': 25, 'mutation_rate': 0.30, 'mutation_percentage': 0.3,'elitism_rate': 0.1, 'structure_rate': 0.33}
 		# epochs = 250
